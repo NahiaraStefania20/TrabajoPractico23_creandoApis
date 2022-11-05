2731f4e69ab97f9b5ac9431878126edad73b355f
Nahiara Stefania Belvedere Salinas <nbelvedere@provincianet.com.ar>
2022-11-05 18:12:45 -0300

    subo mis cambios

---
+++ src/routes/moviesRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {list,newest,recomended,detail,create,update,destroy} = require('../controllers/moviesController');
+
+router
+    .get('/movies', list)
+    .get('/movies/new', newest)
+    .get('/movies/recommended', recomended)
+    .get('/movies/:id', detail)
+    .post('/movies', create)
+    .put('/movies/:id', update)
+    .delete('/movies/:id', destroy)
+
+module.exports = router;
